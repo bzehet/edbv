2e375d56935c905bc9d489cdbd991e05d563f93e
Pascal <pkawasser@gmx.at>
2017-12-30 13:22:26 +0100

    GUI output added removed some test files

--- code/main.m
+++ code/main.m
@@ -23,7 +23,7 @@ solutionFilename = 'testset/image_numbers.xlsx';
 
 %load solutions if the provided dataset is used
 if(dataset)
-[num, solution_Formula, raw] = xlsread(solutionFilename, 'B3:C102');
+[num, solution_Formula, raw] = xlsread(solutionFilename, 'B3:C102'); %#ok<ASGLU>
 imageNumber = str2num(strtok(imageName, '.')); %#ok<ST2NM>
     if(isempty(imageNumber))
        error('Falls ein eigener Datensatz verwendet wird darf die Checkbox "Datensatz" nicht markiert werden');
@@ -35,15 +35,11 @@ inputImage = imread(strcat(imagePath, imageName));
 
 %Threshold nach Otsu
 imageOtsu = otsu(inputImage);
-imshow(imageOtsu);
 imageCleaning = cleaning(imageOtsu);
-imshow(imageCleaning);
 
 %Geometrische Transformation
 imageTransformation = imalign(1- imageCleaning, 5);
-imshow(imageTransformation);
 imageFC = fragmentCleaner(imageTransformation);
-imshow(imageFC);
 
 %Connected Component Labeling
 [imageLabeling, boolRot] = labeling(imageFC);
@@ -54,12 +50,7 @@ end
 sizeOf = size(imageLabeling);
 if(sizeOf > 30)
     error('Too many components');
-end
-    
-for i=1:sizeOf(1,3)
-  imshow(imageLabeling(:,:,i));
-end
-    
+end  
 
 %Symbol Recognition
 symbolsFormula = symbolRecognition(imageLabeling(:,:,1:end-1));
@@ -73,14 +64,16 @@ output_formula = calculateFormula(cnn, imageLabeling(:,:,1:end-1), symbolsFormul
 try
     output_result = calculate(output_formula);
 catch
-    output_result = '?';
+    output_result = 'Ergebnis konnte nicht berechnet werden';
 end
 if(dataset)
     output_formula_solution = solution_Formula(imageNumber+1,1);
     output_result_solution = calculate(output_formula_solution{1});
 else
     if(isempty(inputFormula))
-        %can't check if result is correct
+        %can't check result
+        output_formula_solution = 'Es wurde keine Formel eingegeben';
+        output_result_solution = '?';
     else
         if(inputFormula(end) == '=')
            inputFormula = inputFormula(1:end-1); 
@@ -89,6 +82,9 @@ else
         output_result_solution = calculate(output_formula_solution);
     end
 end
+%Placeholder image for labeling
+imagePlaceHolder = imread('testset/placeholder.jpg');
+guiOutput(inputImage, imageOtsu, imageCleaning, imageFC, imagePlaceHolder, output_formula, output_result, output_formula_solution, output_result_solution);
 end
 
 
