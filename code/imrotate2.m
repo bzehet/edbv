31ab1ad454190eea74f2acafd4e2c3bab3edda1c
Björn <e0930808@student.tuwien.ac.at>
2018-01-02 18:05:52 +0100

    rotation output from logical to double

--- code/imrotate2.m
+++ code/imrotate2.m
@@ -14,7 +14,8 @@ function ret = imrotate2(image, degree)
     offset = [min([tl(1), tr(1), bl(1), br(1)]) - 1, min([tl(2), tr(2), bl(2), br(2)]) - 1];
     new_size = [max([tl(3), tr(3), bl(3), br(3)]) - offset(1), max([tl(4), tr(4), bl(4), br(4)]) - offset(2)];
     
-    image_rot = false(new_size(1),new_size(2));
+%     image_rot = false(new_size(1),new_size(2));
+    image_rot = zeros(new_size(1),new_size(2));
     
     for row = 1:image_size(1)
         for col = 1:image_size(2)
--- code/rotation.m
+++ code/rotation.m
@@ -1,15 +1,12 @@
-image = imread('76.JPG');
-b_image = 1- im2bw(image,0.4);
+image = imread('testset/80.JPG');
 
-%image_small = imresize(b_image, 0.06);
+imageBin = otsu(image);
+imageCl = cleaning(imageBin);
+figure
+imshow(imageCl);
 
-b_image_maj = bwmorph(b_image,'majority',Inf);
-
-% image_small = imresize(b_image, 0.1);
-
-% image_org = imrotate(b_image,80);
-
-image_rot = imalign(b_image, -5);
+%Geometrische Transformation
+imageRot = imalign(1- imageCl, 5);
 
 %sum([1 1 1; 0 0 0],2)
 
@@ -18,6 +15,5 @@ image_rot = imalign(b_image, -5);
 
 %image_rot = imrotate(b_image, 90);
 
-
 figure
-imshow(image_rot)
+imshow(imageRot)
